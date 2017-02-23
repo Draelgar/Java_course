6e99f34d65dc8d09e05f484fe6b0278d0f9c2d82
elev <elev@192.168.1.102>
2017-02-23 16:03:58 +0100

    Addad file handling

--- BarberBookingSystem/src/mainPackage/BarberBookingSystem.java
+++ BarberBookingSystem/src/mainPackage/BarberBookingSystem.java
@@ -6,14 +6,24 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TimeZone;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Clock;
-import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class BarberBookingSystem {
-	private Set<BookedTime> mBookings; // A set of bookings.
+	private SortedSet<BookedTime> mBookings; // A set of bookings.
 	private Set<String> mBarbers; // A set of available barbers.
 	private DisplayMode mDisplayMode; // Determines which bookings that should be displayed.
+	private String mCurrentBarber; // The currently used barber.
 	
 	public static void main(String[] args) {
 		BarberBookingSystem bbs = new BarberBookingSystem();
@@ -22,7 +32,7 @@ public class BarberBookingSystem {
 	
 	// Default constructor.
 	public BarberBookingSystem() {
-		mBookings = new HashSet<BookedTime>();
+		mBookings = new TreeSet<BookedTime>();
 		mBarbers = new HashSet<String>();
 		
 		mDisplayMode = DisplayMode.Day;
@@ -30,6 +40,8 @@ public class BarberBookingSystem {
 		// Add some default barbers.
 		mBarbers.add("Lena");
 		mBarbers.add("Olof");
+		
+		mCurrentBarber = "Lena";
 	}
 	
 	// Run the application loop.
@@ -49,17 +61,27 @@ public class BarberBookingSystem {
 			switch(programMode) {
 				case 0:
 				{
-					programMode = menu(); // TODO Menu: Select Task.
+					programMode = menu(); // Select Task.
 					break;
 				}
 				case 1:
 				{
-					programMode = bookTime(); // TODO Menu: Assign Date & Time to Book.
+					programMode = bookTime(); // Assign Date & Time to Book.
 					break;
 				}
 				case 2:
 				{
-					programMode = displayBookings(mDisplayMode); // TODO: Display bookings.
+					programMode = displayBookings(mDisplayMode); // Display bookings.
+					break;
+				}
+				case 3:
+				{
+					programMode = selectBarber();
+					break;
+				}
+				case 4:
+				{
+					programMode = showFreeTime();
 					break;
 				}
 				default:
@@ -69,8 +91,9 @@ public class BarberBookingSystem {
 				}
 			}
 		}
+		
+		save();
 	}
-	
 	
 	// Book a new time for the customer.
 	private int bookTime() {
@@ -79,42 +102,205 @@ public class BarberBookingSystem {
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		
-		String customer;
+		String customer, barber = mCurrentBarber;
 		String response;
-		int year, month, day, startH, startM, endH, endM;
-		char charBuffer[] = new char[8];
+		int year = -1, month = -1, day = -1, startH = -1, startM = -1, durationM = -1;
 		
 		ZonedDateTime startTime;
-		ZonedDateTime endTime;
+		Duration duration;
+		
+		// Setting up the patterns.
+		Pattern patFour = Pattern.compile("(\\d{4})"), 
+				patTwo = Pattern.compile("(\\d{2})");
+		
+		Matcher m;
 		
 		while(running) {
 			System.out.println("What is your name?");
 			customer = scanner.nextLine();
 			
+			// Did the user want to cancel the operation?
+			if(customer.toLowerCase().equals("cancel")) {
+				return 0;
+			}
+			
+			// Ask for the date
 			while(repeat) {
 				System.out.println("Assign the desired date. (YYYY-MM-DD)");
 				response = scanner.nextLine();
 				
-				// Was the date correct?
-				if(response.length() == 10) {
-					response.getChars(0, 4, charBuffer, 0);
-					response.getChars(5, 7, charBuffer, 0);
-					response.getChars(8, 10, charBuffer, 0);
-					
-					for(int i = 0; i < response.length(); i++)
-						if(!Character.isDigit(charBuffer[i])) {
-							
-						}
+				// Did the user want to cancel the operation?
+				if(response.toLowerCase().equals("cancel")) {
+					return 0;
 				}
-				else
+					
+				// Set the matcher to match for 4 numbers in a row.
+				m = patFour.matcher(response);
+				
+				LocalDateTime ldt;
+				
+				repeat = true;
+				
+				// Check for the year.
+				if(m.find()) {
+					year = Integer.parseInt(m.group(0));
+					
+					// Set the matcher to match against double numbers and skip the 4 first values as that was the year.
+					m = patTwo.matcher(response.substring(4));
+					
+					// Check for the month.
+					if(m.find()) {
+						month = Integer.parseInt(m.group(0));
+						
+						// Check for the day.
+						if(m.find()) {
+							day = Integer.parseInt(m.group(0));
+							
+							if(month >= 1 && month <= 12) {	// Is this a valid month?
+								ldt = LocalDateTime.of(1, month, 1, 1, 1, 0, 0);
+								
+								if(day >= 1 && day <= ldt.getMonth().length(false)) {
+									System.out.println(year + "-" + month + "-" + day);
+									repeat = false;
+								}
+								else {
+									System.out.println(day + " is not a valid day! Only values between 01 and " + 
+																			ldt.getMonth().length(false) +" is accepted!");
+									day = -1;
+									repeat = true;
+								}
+							}
+							else {
+								System.out.println(month + " is not a valid month! Only values between 01 and 12 is accepted!");
+								day = -1;
+								repeat = true;
+							}
+						}
+					}
+				}
+				
+				if(day == -1)
+				{
+					System.out.println("The date format was incorrect, please try again!");
 					repeat = true;
+				}
+				
 			}
+			
+			// Ask for the duration.
+			repeat = true;
+			while(repeat) {
+				System.out.println("Select a duration in minutes (20 - 99). (mm)");
+				
+				response = scanner.nextLine();
+				
+				// Did the user want to cancel the operation?
+				if(response.toLowerCase().equals("cancel")) {
+					return 0;
+				}
+				
+				// Set the matcher to match against double numbers and skip the 4 first values as that was the year.
+				m = patTwo.matcher(response);
+				
+				// Get the value.
+				if(m.find()) {
+					durationM = Integer.parseInt(m.group(0));
+					
+					if(durationM <= 99 && durationM >= 20) {
+						repeat = false;
+					}
+					else {
+						System.out.println("The duration was not long enough or too long!");
+						repeat = true;
+					}
+				}
+				
+				if(durationM == -1)
+				{
+					System.out.println("The duration format was incorrect, please try again!");
+					repeat = true;
+				}
+			}
+			
+			duration = Duration.ofMinutes(durationM);
+			
+			// Ask for the start time.
+			repeat = true;
+			while(repeat) {
+				System.out.println("Select a start time. (hh:mm)");
+				
+				response = scanner.nextLine();
+				
+				// Did the user want to cancel the operation?
+				if(response.toLowerCase().equals("cancel")) {
+					return 0;
+				}
+					
+				// Set the matcher to match for a pair or numbers.
+				m = patTwo.matcher(response);
+				
+				repeat = true;
+				
+				// Find the hours,
+				if(m.find()) {
+					startH = Integer.parseInt(m.group(0));
+					
+					// Find the minutes.
+					if(m.find()) {
+						startM = Integer.parseInt(m.group(0));
+						
+						if(startH >= 8 && startH <= 18 - duration.toHours()) { // Is this hour valid?
+							if(startM >= 0 && startM <= 59) { // Is this a valid minute?
+								System.out.println(startH + ":" + startM);
+								repeat = false;
+							}
+							else {
+								System.out.println(startM + " is not accepted! Only hours between 8 and 18 can be booked!");
+								repeat = true;
+								startM = -1;
+							}
+						}
+						else {
+							System.out.println(startH + " is not a valid hour! Only values 0 to 23 is accepted!");
+							repeat = true;
+							startM = -1;
+						}
+					}
+				}
+
+				if(startM == -1)
+				{
+					System.out.println("The start time format was incorrect, please try again!");
+				}
+						
+				// Combine the data into a single ZonedDateTime object.
+				startTime = ZonedDateTime.of(year, month, day, startH, startM, 0, 0, Calendar.getInstance().getTimeZone().toZoneId());
+				
+				repeat = false; // Everything looks okay so far.
+				
+				// Loop through the list.
+				Iterator<BookedTime> it = mBookings.iterator();
+				
+				while(it.hasNext()) {
+					BookedTime b = it.next();
+					
+					// Check for overlaps!
+					if(b.isOverlaping(mCurrentBarber, startTime, startTime.plusMinutes(duration.toMinutes()))) {
+						System.out.println("The selected time is already in use!");
+						repeat = true;
+						break;
+					}
+				}
+				// Was the time acceptable?
+				if(!repeat) {
+					mBookings.add(new BookedTime(startTime, duration, customer, barber));
+					
+					System.out.println("Appointment booked successfully!");
+					running = false;
+				}
+			}
+			
 		}
-		// TODO: Ask for customer name.
-		// TODO: Ask for a date.
-		// TODO: Ask for a new time booking & barber.
-		// TODO: If incorrect time was given or if the time overlaps another with the same barber, retry.
-		// TODO: Typing exit at any time cancels the booking process ahead of time.
 		
 		return 0;
 	}
@@ -126,32 +312,28 @@ public class BarberBookingSystem {
 		
 		String response = "";
 		
-		System.out.println("What do you wish to do?\n-See bookings for today (d)\n-See bookings for this week (w)\n-See bookings for this month(m)\n"
-				+ "-See bookings for this year (y)\n-Book a new time (b)\n-Exit (exit)");
+		System.out.println("What do you wish to do?\n-See current bookings (c)\n-See available time (a)\n" +
+				"-Book a new time (b)\n-Exit (exit)\n" + 
+				"-Select barber (s)\nYou can cancel at any time and go back here by typing (cancel)");
 		
 		response = scanner.nextLine();
 		response = response.toLowerCase();
 		
 		if(response.length() > 0) {
 			for(int i = 0; i < response.length(); i++) {
-				if(response.startsWith("d")) {
-					mDisplayMode = DisplayMode.Day;
+				if(response.startsWith("c")) {
+					setDisplayMode();
 					return 2;
 				}
-				else if(response.startsWith("w")) {
-					mDisplayMode = DisplayMode.Week;
-					return 2;
-				}
-				else if(response.startsWith("m")) {
-					mDisplayMode = DisplayMode.Month;
-					return 2;
-				}
-				else if(response.startsWith("y")) {
-					mDisplayMode = DisplayMode.Year;
-					return 2;
+				else if(response.startsWith("a")) {
+					setDisplayMode();
+					return 4;
 				}
 				else if(response.startsWith("b")) {
 					return 1;
+				}
+				else if(response.startsWith("s")) {
+					return 3;
 				}
 				else if(response.equals("exit")) {
 					return -1;
@@ -166,7 +348,46 @@ public class BarberBookingSystem {
 		return 0;
 	}
 	
-	private boolean isIncluded(ZonedDateTime zdt)
+	private void setDisplayMode() {
+		boolean repeat = true;
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		
+		String response = "";
+		
+		while(repeat) {
+			System.out.println("For which time do you wich to see?\n-Today (d)\n-This week (w)\n-This month(m)\n"
+					+ "-This year (y)\nYou can cancel at any time and go back here by typing (cancel)");
+			
+			response = scanner.nextLine();
+			response = response.toLowerCase();
+			
+			if(response.equals("cancel"))
+				return;
+			
+			if(response.length() > 0) {
+				repeat = false;
+				if(response.startsWith("d")) {
+					mDisplayMode = DisplayMode.Day;
+				}
+				else if(response.startsWith("w")) {
+					mDisplayMode = DisplayMode.Week;
+				}
+				else if(response.startsWith("m")) {
+					mDisplayMode = DisplayMode.Month;
+				}
+				else if(response.startsWith("y")) {
+					mDisplayMode = DisplayMode.Year;
+				}
+				else {
+					System.out.println("Invalid option!");
+					repeat = true;
+				}
+			}
+		}
+	}
+	
+ 	private boolean isIncluded(ZonedDateTime zdt)
 	{
 		ZonedDateTime time = ZonedDateTime.now(zdt.getZone()); // Get the current time for this zone ID.
 		ZonedDateTime limit;
@@ -200,7 +421,7 @@ public class BarberBookingSystem {
 					if(utcInt == zdtInt) {
 						// Calculate the time by the end of the the current week.
 						limit = time.plusDays(7 - time.getDayOfWeek().getValue());
-						
+
 						// Is the booked time after the current day and on the same week?
 						if(zdt.isBefore(limit) && zdt.isAfter(time))
 							return true;
@@ -242,15 +463,16 @@ public class BarberBookingSystem {
 		
 		if(mBookings.size() > 0)
 		{
-			System.out.print("Current appointment for this " + mDisplayMode.toString() + ":");
+			System.out.print("Current appointment for this " + mDisplayMode.toString() + " for " + mCurrentBarber + ":\n");
 			
 			Iterator<BookedTime> it = mBookings.iterator();
 			while(it.hasNext()) {
 				BookedTime booking = it.next();
-				if(isIncluded(booking.getStartTime()) || isIncluded(booking.getEndTime())) {
-					booking.print();
-					System.out.println(); // Add an extra empty row.
-				}
+				if(booking.getBarber().equals(mCurrentBarber))	
+					if(isIncluded(booking.getStartTime()) || isIncluded(booking.getEndTime())) {
+						booking.print();
+						System.out.println(); // Add an extra empty row.
+					}
 			}
 		}
 		else
@@ -261,12 +483,109 @@ public class BarberBookingSystem {
 		return 0;
 	}
 	
-	public void AddBarber(String barber) {
+	public void addBarber(String barber) {
 		mBarbers.add(barber);
 	}
 	
-	public void RemoveBarber(String barber) {
+	public void removeBarber(String barber) {
 		mBarbers.remove(barber);
+	}
+	
+	public int selectBarber() {
+		boolean repeat = true;
+		
+		while(repeat) {
+			System.out.println("These are the barbers that work here:");
+			
+			Iterator<String> it = mBarbers.iterator();
+			
+			while(it.hasNext()) {
+				System.out.println("\t-" + it.next());
+			}
+			
+			System.out.println("\nWhich one do you pick?");
+			
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+			
+			String barber = scanner.nextLine();
+			barber = barber.toLowerCase();
+			
+			if(barber.equals("cancel")) {
+				repeat = false;
+				break;
+			}
+			
+			it = mBarbers.iterator();
+			
+			while(it.hasNext()) {
+				String name = it.next();
+				
+				if(name.toLowerCase().equals(barber)) {
+					mCurrentBarber = name;
+					System.out.println(mCurrentBarber + " is now the active barber.");
+					
+					repeat = false;
+				}
+			}
+			
+			if(!mCurrentBarber.toLowerCase().equals(barber)) {
+				System.out.println("Barber does not exist! Going with " + mCurrentBarber + " instead!");
+			}
+		}
+		
+		return 0;
+	}
+	
+	private int showFreeTime() {
+		
+		System.out.print("Current unbooked time for " + mCurrentBarber + ":\n");
+		
+		// Assuming the list is in order.
+		System.out.print("08:00 - ");
+		
+		Iterator<BookedTime> it = mBookings.iterator();
+		
+		while(it.hasNext()) {
+			BookedTime booking = it.next();
+			if(booking.getBarber().equals(mCurrentBarber))	
+				if(isIncluded(booking.getStartTime()) || isIncluded(booking.getEndTime())) {
+					
+					// Assuming the list is in order.
+					System.out.println(booking.getStartTime().minusMinutes(1).toLocalTime().toString());
+					System.out.print(booking.getEndTime().plusMinutes(1).toLocalTime().toString() + " - ");
+					
+				}
+		}
+		
+		System.out.println("18:00");
+		
+		return 0;
+	}
+	
+	private void save() {
+		
+		File file = new File("data/data.txt");
+		
+		try {
+			if(!file.exists()) // If the file does not exist, create a new one.
+				file.createNewFile();
+			
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			bw.write(""); // Overwrite the old data.
+			
+			for(BookedTime bt : mBookings) {
+				bt.save(bw);
+			}
+			
+			bw.close();
+			
+		} catch (IOException e) {
+			System.out.println("Error, failed to create the file! " + e.getMessage());
+		}
+		
 	}
 
 }
--- BarberBookingSystem/src/mainPackage/BookedTime.java
+++ BarberBookingSystem/src/mainPackage/BookedTime.java
@@ -1,30 +1,35 @@
 package mainPackage;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.time.Duration;
 import java.time.ZonedDateTime;
 
-public class BookedTime {
+@SuppressWarnings("rawtypes")
+public class BookedTime implements Comparable{
 	private ZonedDateTime mStartTime;
-	private ZonedDateTime mEndTime;
+	private Duration mDuration;
 	private String mCustomer;
 	private String mBarber;
 	
 	public BookedTime() {
 		mStartTime = ZonedDateTime.now();
-		mEndTime = ZonedDateTime.now();
+		mDuration = Duration.ofHours(1);
+
 		mCustomer = "None";
 		mBarber = "None";
 	}
 	
-	public BookedTime(ZonedDateTime start, ZonedDateTime end, String customer, String barber) {
+	public BookedTime(ZonedDateTime start, Duration duration, String customer, String barber) {
 		mStartTime = start;
-		mEndTime = end;
+		mDuration = duration;
 		mCustomer = customer;
 		mBarber = barber;
 	}
 	
-	public void setTimeInterval(ZonedDateTime start, ZonedDateTime end) {
+	public void setTimeInterval(ZonedDateTime start, Duration duration) {
 		mStartTime = start;
-		mEndTime = end;
+		mDuration = duration;
 	}
 	
 	public void setBarber(String barber) {
@@ -40,7 +45,14 @@ public class BookedTime {
 	}
 	
 	public ZonedDateTime getEndTime() {
-		return mEndTime;
+		ZonedDateTime endTime = mStartTime;
+		endTime = endTime.plusMinutes(mDuration.toMinutes());
+		
+		return endTime;
+	}
+	
+	public Duration getDuration() {
+		return mDuration;
 	}
 	
 	public String getCustomer() {
@@ -52,12 +64,22 @@ public class BookedTime {
 	}
 	
 	// Check weather the given time interval overlaps the booked one.
-	public boolean isOverlaping(ZonedDateTime start, ZonedDateTime end) {
-		if((start.isAfter(mStartTime) && start.isBefore(mEndTime)) || 
-				(end.isAfter(mStartTime) && end.isBefore(mEndTime)))
+	public boolean isOverlaping(String barber, ZonedDateTime start, ZonedDateTime end) {
+		if(mBarber.equals(barber) && ((start.isAfter(mStartTime) && start.isBefore(getEndTime())) || 
+				(end.isAfter(mStartTime) && end.isBefore(getEndTime()) || end.isEqual(mStartTime) || 
+						end.isEqual(getEndTime()) || start.isEqual(mStartTime) || start.isEqual(getEndTime()))))
 			return true;
 		
 		return false;
+	}
+	
+	public int compareTo(Object o) {
+		
+		BookedTime bt = (BookedTime)o;
+		if(bt.getStartTime().isAfter(mStartTime))
+			return -1;
+		else
+			return 1;
 	}
 	
 	public void print() {
@@ -65,10 +87,20 @@ public class BookedTime {
 		System.out.println("Barber: " + mBarber);
 		System.out.println("Customer: " + mCustomer);
 		
-		// Convert time to a string representation and cut the seconds.
-		String start = mStartTime.toLocalTime().toString().substring(0, 7);
-		String end = mEndTime.toLocalTime().toString().substring(0, 7);
+		// Convert time to a string representation.
+		String start = mStartTime.toLocalTime().toString().substring(0);
+		String end = getEndTime().toLocalTime().toString().substring(0);
 		
 		System.out.println("Time: " + start + " - " + end);
+	}
+	
+	public void save(BufferedWriter bw) {
+		try {
+		bw.append(mBarber + "," + mCustomer + "," + mStartTime.toString() + "," + mDuration.toMinutes());
+		bw.newLine();
+		
+		} catch(IOException e) {
+			System.out.println("Error! " + e.getMessage());
+		}
 	}
 }
