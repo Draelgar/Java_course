c3fec79dceff9555e14b9e3e08bc789433a434a8
Draelgar <g.peter.hultgren@gmail.com>
2017-03-27 16:00:29 +0200

    Added calculator logic.

--- HTML5Proj/WebContent/src/script.js
+++ HTML5Proj/WebContent/src/script.js
@@ -18,11 +18,47 @@ function submit() {
 	document.getElementById("output").innerHTML = name;
 }
 
+function divide(string) {
+	var values = string.split("/");
+	
+	if(values.length > 0)
+	var sum = parseFloat(values[0]);
+	
+	for(i = 1; i < values.length; i++) {
+		sum /= parseFloat(values[i]);
+	}
+	
+	return sum;
+}
+
+function mult(string) {
+	var values = string.split("*");
+	var sum = 1.0;
+	
+	for(i = 0; i < values.length; i++) {
+		if(values[i].indexOf("/") != -1) {
+			sum *= divide(values[i]);
+		}
+		else {
+			sum *= parseFloat(values[i]);
+		}
+	}
+	
+	return sum;
+}
+
 function calc() {
-	var text = document.getElementById("calc").value.split("+");
-	var a = parseFloat(text[0]);
-	var b = parseFloat(text[1]);
-	var sum = a + b;
+	var plus = document.getElementById("calc").value.split("+");
+	var sum = 0.0;
+	
+	for(i = 0; i < plus.length; i++) {
+		if(plus[i].indexOf("*") != -1) {
+			sum += mult(plus[i]);
+		}
+		else {
+			sum += parseFloat(plus[i]);
+		}
+	}
 	
 	document.getElementById("sum").innerHTML = sum;
 }
